1508681464282549ed5e6ccde9208ea65573c777
Mabroor <mabroorahmed18@gmail.com>
2024-06-19 18:55:29 +0100

    sliding cards added using marquee

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import demoGif from "../public/demo-vid-ezgif.com-video-to-gif-converter.gif"
 
 
 import TextAnimation from "@/components/textAnimation";
+import Marquee from "@/components/magicui/marquee";
+import MovingCards from "@/components/movingCards";
 
 
 export default function Home() {
@@ -14,33 +16,23 @@ export default function Home() {
   return (
     <>
       
-      <div className="flex flex-col justify-center md:flex-col lg:justify-center sm:flex-col lg:py-20 ">
-        <div className="flex flex-col justify-center  ">
-          <div className="text-center text-black text-middle  ">
+      <div className="flex flex-col justify-center py-20 md:flex-col lg:justify-center sm:flex-col lg:py-20 ">
+        <div className="flex flex-col justify-center ">
+          <div className="text-center text-black text-middle ">
             
             
-            <h1 className="text-5xl font-extrabold  ">
-
-                Bypass
-                <TextAnimation/>
-                AI DETECTION
-            </h1>
+            <div className="text-5xl font-extrabold ">
+              Breathe life into your videos
+            </div>
             <p className="py-3 text-gray-500 lg:py-3 lg:pt-8 lg:text-lg sm:py-1 sm:pt-2 ">
-            Save hours of writing in a click.
+            Generate Any Sound from Text, No More Extracting from Clips
             </p>
             
           </div>
-          <div className="flex  justify-center">
-            <Button size="xlg" variant="default" className="text-2xl text-black rounded-lg bg-clip bg-gradient-to-r from-teal-400 to-violet-400 ">
-                Get Humanize
-            </Button>
-          </div>
         </div>
-        
-
-
       </div>
       
+      <MovingCards/>
 
     </>
   );
---
+++ components/movingCards.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import Marquee from './magicui/marquee'
+
+function MovingCards() {
+  return (
+    <>
+    
+        {/* // Cards */}
+        <div className='flex flex-row gap-6 '>
+            {/* CARD */}
+            <Marquee pauseOnHover  className=''>
+            <div className='h-fit w-fit'>
+                <div className=' rounded-3xl h-36 w-36 bg-slate-400'>
+
+                </div>
+                <div className='pt-2'>
+                    <div  className='text-sm text-gray-600 '>Prompt</div>
+                    <div className='text-base text-black max-w-36'>Shotgun sound</div>
+                </div>
+            </div>
+            {/* CARD */}
+            <div className='h-fit w-fit'>
+                <div className=' rounded-3xl h-36 w-36 bg-slate-400'>
+
+                </div>
+                <div className='pt-2'>
+                    <div  className='text-sm text-gray-600 '>Prompt</div>
+                    <div className='text-base text-black max-w-36'>Glass clicking in a disco party</div>
+                </div>
+            </div>
+
+            {/* CARD */}
+            <div className='h-fit w-fit'>
+                <div className=' rounded-3xl h-36 w-36 bg-slate-400'>
+
+                </div>
+                <div className='pt-2'>
+                    <div  className='text-sm text-gray-600 '>Prompt</div>
+                    <div className='text-base text-black max-w-36'>A man screaming then falling down stairs</div>
+                </div>
+            </div>
+            {/* CARD */}
+            <div className='h-fit w-fit'>
+                <div className=' rounded-3xl h-36 w-36 bg-slate-400'>
+
+                </div>
+                <div className='pt-2'>
+                    <div  className='text-sm text-gray-600 '>Prompt</div>
+                    <div className='text-base text-black max-w-36'>A man screaming then falling down stairs</div>
+                </div>
+            </div>
+            {/* CARD */}
+            <div className='h-fit w-fit'>
+                <div className=' rounded-3xl h-36 w-36 bg-slate-400'>
+
+                </div>
+                <div className='pt-2'>
+                    <div  className='text-sm text-gray-600 '>Prompt</div>
+                    <div className='text-base text-black max-w-36'>A man screaming</div>
+                </div>
+            </div>
+            </Marquee>
+        </div>
+
+    </>
+  )
+}
+
+export default MovingCards
