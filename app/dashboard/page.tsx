86700a395518162fe723f9503a4c6dcc0e713da4
Mabroor <mabroorahmed18@gmail.com>
2024-06-19 12:51:31 +0100

    play button removed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { generateSounds, getUserCredits, reduceUserCredits } from '../actions';
 import { Button } from '@/components/ui/button';
 
@@ -8,11 +8,11 @@ const Dashboard = () => {
   
     const [inputText, setInputText] = useState<string>('');
     const [audioData, setAudioData] = useState<string | null>(null);
-    const [audioPlayer, setAudioPlayer] = useState<HTMLAudioElement | null>(null);
+    const audioPlayerRef = useRef<HTMLAudioElement | null>(null);
 
     const handleGenerateSound = async () => {
       try {
-        const durationSeconds = 3; // Adjust based on your needs
+        const durationSeconds = 3; 
         const promptInfluence = 0.5; // Adjust based on your needs
   
         const response = await generateSounds(
@@ -30,17 +30,12 @@ const Dashboard = () => {
       }
     };
 
-    const handlePlayAudio = () => {
-      if (audioData && audioPlayer) {
+    useEffect(() => {
+      if (audioData && audioPlayerRef.current) {
         const audioSrc = `data:audio/mpeg;base64,${audioData}`;
-        audioPlayer.src = audioSrc;
-        audioPlayer.play().catch((error) => {
-          console.error("Failed to play audio:", error);
-        });
-      } else {
-        console.log("No audio data available");
+        audioPlayerRef.current.src = audioSrc;
       }
-    };
+    }, [audioData]);
 
     return (
       <div className="flex flex-col items-center justify-center p-4 min-h-80">
@@ -55,19 +50,14 @@ const Dashboard = () => {
       {audioData && (
         <div className="w-full max-w-md mt-6 text-center">
           <h2 className="mb-4 text-xl font-semibold">Generated Sound</h2>
-          <button
-            onClick={handlePlayAudio}
-            className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-          >
-            Play Audio
-          </button>
-          <audio
-            ref={(ref) => setAudioPlayer(ref)}
-            controls
-            style={{ marginTop: "10px" }}
-          >
-            Your browser does not support the audio element.
-          </audio>
+
+            <audio
+              ref={audioPlayerRef}
+              controls
+            >
+              Your browser does not support the audio element.
+            </audio>
+
         </div>
       )}
     </div>
