e6450c7ba6866a680343adbaf45dcee414fa30d4
Mabroor Ahmed <ma942@student.le.ac.uk>
2024-06-20 18:58:27 +0100

    history table added

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,4 +1,10 @@
+import { TableCaption, TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from '@/components/ui/table'
+
 import React from 'react'
+
+type SoundEffectsHistory = {
+    array:[],
+}
 
 function Page() {
   return (
@@ -6,6 +12,24 @@ function Page() {
         <div className='flex justify-center text-4xl font-extrabold'>
             History
         </div>
+        <Table>
+            <TableCaption>A list of your generated Sound effects.</TableCaption>
+            <TableHeader className=' hover:bg-transparent group-hover:bg-transparent'>
+                <TableRow>
+                <TableHead className="w-[100px]">Prompt</TableHead>
+    
+                <TableHead className=" text-right">Date</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                <TableCell className="whitespace-nowrap font-medium">cat sounds</TableCell>
+
+                <TableCell className=" text-right">June 19</TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+
     </div>
   )
 }
