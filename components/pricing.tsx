ef525c898d5102a0174df3808c08b387cf4eee74
Mabroor <mabroorahmed18@gmail.com>
2024-06-30 19:55:05 +0100

    Pricing cards updated

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 
 
 
+
 interface Plan {
     link: string;
     price: string;
@@ -55,7 +56,7 @@ export default function Pricing() {
 
   return (
     <>
-    <div className='flex flex-col justify-center text-center text-black '>
+    <div className='flex flex-col justify-center text-center text-black'>
         <div className='p-6 text-5xl font-black text-teal-800'>
             Pricing
         </div>
@@ -66,24 +67,27 @@ export default function Pricing() {
 
         {/* Switch from yearly to monthly */}
         <div className="space-x-2">
-        <Button 
-          className={` ${planType === "monthly" ? "bg-teal-500 text-white" : ""}`}
+        <Button
+          variant="outline"
+          className={` ${planType === "monthly" ? "bg-teal-500 text-white" : "border border-black"} `}
           onClick={() => togglePlanType("monthly")}
         >
           Monthly
         </Button>
-        <Button 
-          className={` ${planType === "yearly" ? "bg-teal-500 text-white" : ""}`}
+        <Button
+          variant="outline" 
+          className={` ${planType === "yearly" ? "bg-teal-500 text-white" : "border border-black"} `}
           onClick={() => togglePlanType("yearly")}
         >
           Yearly
         </Button>
       </div>
         {/* PRICING CARDS */}
-        <div className='flex flex-col justify-center pb-12 space-y-4 md:space-y-0 md:justify-around md:flex-row md:w-full'>
+        <div className='flex flex-col justify-center py-12 space-y-4 md:space-y-0 md:justify-around md:flex-row md:w-full'>
 
             {/* STARTER CARD */}
-            <div className="bg-[#212121] border rounded-lg flex flex-col justify-end w-4/5 md:w-1/3 mx-auto md:justify-normal text-left p-5 md:transition-transform md:transform md:hover:scale-105">
+            <div className="bg-[#212121]  border rounded-lg flex flex-col justify-end w-3/5 md:w-[350px] mx-auto md:justify-normal text-left p-5 md:transition-transform md:transform md:hover:scale-105">
+              
                 <h4 className="text-teal-500">Starter</h4>
                 <div className="flex flex-row">
                     {/* <p className="mt-6 text-gray-400 line-through">£39.99</p> */}
@@ -102,7 +106,7 @@ export default function Pricing() {
                     </p>
                     <p className="flex flex-row items-center py-3">
                         <Check size={28} color="white" weight="regular" />
-                        <span className="ml-3">1 sounds generated per request</span>
+                        <span className="ml-3">Single sound generated per request</span>
                     </p>
 
                     <p className="flex flex-row items-center py-3 text-gray-500">
@@ -117,10 +121,11 @@ export default function Pricing() {
             </div>
             
             {/* Pro CARD */}
-            <div className="bg-[#212121] border-2 border-teal-400 rounded-lg flex flex-col w-4/5 md:w-1/3 mx-auto md:justify-normal text-left p-5 md:transition-transform md:transform md:hover:scale-105">
+            <div className="bg-[#212121] border-2 border-teal-400 rounded-lg flex flex-col w-3/5 md:w-[400px] mx-auto md:justify-normal text-left p-5 md:transition-transform md:transform md:hover:scale-105">
+
                 <h4 className="text-teal-500">Pro</h4>
                 <div className="flex flex-row">
-                    <p className="mt-6 text-gray-400 line-through">29.99</p>
+                    <p className="mt-6 text-gray-400 line-through">{planType === "yearly" ? "£29.99" : "£34.99"}</p>
                     <h1 className='text-4xl font-black text-teal-300 md:text-5xl bg-inherit'>
                     {planType === "yearly" ? "£19.99" : "£24.99"}
                     </h1>
@@ -129,7 +134,7 @@ export default function Pricing() {
                    
                     <p className="flex flex-row items-center py-3">
                         <Check size={28} color="#42BEA0" weight="regular" />
-                        <span className="ml-3">100 Uses/month</span>
+                        <span className="ml-3">500 Uses/month</span>
                     </p>
                     <p className="flex flex-row items-center py-3">
                         <Check size={28} color="#42BEA0" weight="regular" />
@@ -151,7 +156,40 @@ export default function Pricing() {
                     Buy now
                 </Button>
             </div>
+            
+            {/* PREMIUM CARD */}
+            <div className="bg-[#212121] border rounded-lg flex flex-col justify-end w-3/5 md:w-[350px] mx-auto md:justify-normal text-left p-5 md:transition-transform md:transform md:hover:scale-105">
+                <h4 className="text-teal-500">Premium</h4>
+                <div className="flex flex-row">
+                    {/* <p className="mt-6 text-gray-400 line-through">£39.99</p> */}
+                    <h1 className='text-4xl font-black text-white md:text-5xl bg-inherit'>
+                    {planType === "yearly" ? "£39.99" : "£34.99"}
+                    </h1>
+                </div>
+                <div className="py-4 text-white">
+                    <p className="flex flex-row items-center py-3">
+                        <Check size={28} color="white" weight="regular" />
+                        <span className="ml-3">1000 Uses/month </span>
+                    </p>
+                    <p className="flex flex-row items-center py-3">
+                        <Check size={28} color="white" weight="regular" />
+                        <span className="ml-3">High quality image input</span>
+                    </p>
+                    <p className="flex flex-row items-center py-3">
+                        <Check size={28} color="white" weight="regular" />
+                        <span className="ml-3">3 sounds generated per request</span>
+                    </p>
 
+                    <p className="flex flex-row items-center py-3">
+                        <Check size={28} color="white" weight="regular" />
+                        <span className="ml-3">Access to generated sound history</span>
+                    </p>
+
+                </div>
+                <Link href={`${plans[planType][0].link}`}>
+                    <Button className="w-full px-4 text-2xl font-medium text-white bg-teal-500 md:min-w-64">Buy now</Button>
+                </Link>
+            </div>
         </div>
 
     </div>
